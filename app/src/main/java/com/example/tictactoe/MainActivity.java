c6c819be25ff82ac294e651ebed2031f4ad87886
gmesh <gmeshie@gmail.com>
2022-12-04 20:07:44 +0200

    Add game logic and win marks

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -10,15 +10,30 @@ public class MainActivity extends AppCompatActivity {
 
     boolean isGameActive = true;
 
-    // Indications: 0-player X, 1-player O, 2-otherwise (not chosen yet)
+    // Indications: 0-player X, 1-player O, 2-otherwise (tile not pressed yet)
 
     int activePlayer = 0;
 
     int[] tilesState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
-    int[][] winSituations= {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
-                            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
-                            {0, 4, 8}, {2, 4, 6}};
+    int[][] winSituations= {{0, 1, 2},
+                            {0, 3, 6},
+                            {0, 4, 8},
+                            {1, 4, 7},
+                            {2, 4, 6},
+                            {2, 5, 8},
+                            {3, 4, 5},
+                            {6, 7, 8}};
+
+    int[] winImageRes = {R.drawable.mark6,
+                         R.drawable.mark3,
+                         R.drawable.mark1,
+                         R.drawable.mark4,
+                         R.drawable.mark2,
+                         R.drawable.mark5,
+                         R.drawable.mark7,
+                         R.drawable.mark8};
+
 
     public static int tapsCounter = 0;
 
@@ -57,15 +72,19 @@ public class MainActivity extends AppCompatActivity {
 
         boolean isWin = false;
 
-        for (int[] winSituation : winSituations) {
-            // Win
-            if (tilesState[winSituation[0]] == tilesState[winSituation[1]] &&
-                tilesState[winSituation[1]] == tilesState[winSituation[2]] &&
-                tilesState[winSituation[0]] != 2) {
-                isWin = true;
+        ImageView winMark = findViewById(R.id.winMark);
 
+        for (int i = 0; i <= winSituations.length; i++) {
+            // When a player tapped on all of a win situation's tiles
+            if (tilesState[winSituations[i][0]] == tilesState[winSituations[i][1]] &&
+                tilesState[winSituations[i][1]] == tilesState[winSituations[i][2]] &&
+                tilesState[winSituations[i][0]] != 2) {
+                isWin = true;
                 isGameActive = false;
-                if (tilesState[winSituation[0]] == 0) {
+
+                winMark.setImageResource(winImageRes[i]);
+
+                if (tilesState[winSituations[i][0]] == 0) {
                     gameStatus.setImageResource(R.drawable.xwin);
                 } else {
                     gameStatus.setImageResource(R.drawable.owin);
@@ -79,23 +98,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // reset the game
     public void gameReset(View view) {
         isGameActive = true;
         activePlayer = 0;
+
         for (int i = 0; i < tilesState.length; i++) {
             tilesState[i] = 2;
         }
-        // remove all the images from the boxes inside the grid
-        ((ImageView) findViewById(R.id.tile0)).setImageResource(0);
-        ((ImageView) findViewById(R.id.tile1)).setImageResource(0);
-        ((ImageView) findViewById(R.id.tile2)).setImageResource(0);
-        ((ImageView) findViewById(R.id.tile3)).setImageResource(0);
-        ((ImageView) findViewById(R.id.tile4)).setImageResource(0);
-        ((ImageView) findViewById(R.id.tile5)).setImageResource(0);
-        ((ImageView) findViewById(R.id.tile6)).setImageResource(0);
-        ((ImageView) findViewById(R.id.tile7)).setImageResource(0);
-        ((ImageView) findViewById(R.id.tile8)).setImageResource(0);
+
+        ((ImageView) findViewById(R.id.tile0)).setImageResource(R.drawable.empty);
+        ((ImageView) findViewById(R.id.tile1)).setImageResource(R.drawable.empty);
+        ((ImageView) findViewById(R.id.tile2)).setImageResource(R.drawable.empty);
+        ((ImageView) findViewById(R.id.tile3)).setImageResource(R.drawable.empty);
+        ((ImageView) findViewById(R.id.tile4)).setImageResource(R.drawable.empty);
+        ((ImageView) findViewById(R.id.tile5)).setImageResource(R.drawable.empty);
+        ((ImageView) findViewById(R.id.tile6)).setImageResource(R.drawable.empty);
+        ((ImageView) findViewById(R.id.tile7)).setImageResource(R.drawable.empty);
+        ((ImageView) findViewById(R.id.tile8)).setImageResource(R.drawable.empty);
 
         ImageView gameStatus = findViewById(R.id.gameStatus);
         gameStatus.setImageResource(R.drawable.xplay);
